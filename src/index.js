8c0b4142d70d15ea2b16517ca0cc50fe49458699
amandafdal <amandafdal@gmail.com>
2020-04-02 20:38:17 -0300

    manipulação DOM

--- src/index.js
+++ src/index.js
@@ -1,3 +1,18 @@
-import cipher from './cipher.js';
+//import cipher from './cipher.js';
+//console.log(cipher);
 
-console.log(cipher);
+let textOutput = "isso aqui vai ser o texto cifrado/decifrado";
+document.getElementById("textoutput").innerHTML = (textOutput);
+
+document.getElementById("textinput").addEventListener("input", saveInfo);
+document.getElementById("offset").addEventListener("input" , saveInfo);
+
+function saveInfo() {
+  let textInput = document.getElementById("textinput").value.toUpperCase();
+  let offset = Number(document.getElementById("offset").value);
+console.log(textInput);
+console.log(offset);
+}
+
+//document.getElementById("encode").addEventListener("click", encode);
+//document.getElementById("offset").addEventListener("click", decode);
