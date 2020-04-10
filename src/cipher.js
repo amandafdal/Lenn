ea909e52c64851cf3e07aa9ebf77992754a166b5
amandafdal <amandafdal@gmail.com>
2020-04-09 22:07:20 -0300

    quinto commit. style iPad e desktop + ajustes teste

--- src/cipher.js
+++ src/cipher.js
@@ -1,18 +1,26 @@
 const cipher = {
   
   encode: (shift, word) => { 
+    if (shift =="" || typeof shift !== "number" || typeof word !== "string"){
+      throw new TypeError ("Você precisa inserir uma informação válida.");
+    }
+
     let result= "";
     for (let letter = 0; letter < word.length; letter++) {
-      let encoded=((word[letter].charCodeAt() - 65 + Number(shift)) % 26) + 65;
+      let encoded=((word[letter].charCodeAt() - 65 + shift) % 26) + 65;
       result = result + String.fromCharCode(encoded);
     }  
     return result;
   },  
 
-  decode: (shift, word) => {
+  decode: (shift, word) => {  
+    if (shift =="" || typeof shift !== "number" || typeof word !== "string"){
+      throw new TypeError ("Você precisa inserir uma informação válida.");
+    }
+
     let result= "";
     for (let letter = 0; letter < word.length; letter++) {
-      let decoded=((word[letter].charCodeAt() + 65 - Number(shift)) % 26) + 65;
+      let decoded=((word[letter].charCodeAt() + 65 - shift) % 26) + 65;
       result = result + String.fromCharCode(decoded);
     }  
     return result;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import cipher from './cipher.js';
-console.log(cipher);
 
 document.getElementById("encode").addEventListener("click", function(event){
 
   event.preventDefault();
 
   let textInput=document.getElementById("textinput").value.toUpperCase();
-  let offsetNumber=document.getElementById("offset").value;
+  let offsetNumber=Number(document.getElementById("offset").value);
 
   let output= cipher.encode(offsetNumber, textInput);
   document.getElementById("textoutput").innerHTML= output;
@@ -18,7 +17,7 @@ document.getElementById("decode").addEventListener("click", function(event){
   event.preventDefault();
 
   let textInput=document.getElementById("textinput").value.toUpperCase();
-  let offsetNumber=document.getElementById("offset").value;
+  let offsetNumber=Number(document.getElementById("offset").value);
 
   let output= cipher.decode(offsetNumber, textInput);
   document.getElementById("textoutput").innerHTML= output;
