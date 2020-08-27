5804c0d809362d60aa687550d495d672a698712d
amandafdal <amandafdal@gmail.com>
2020-08-27 20:18:02 -0300

    encode and decode lower case and non alphabetical

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ document.getElementById("encode").addEventListener("click", function(event){
 
   event.preventDefault();
 
-  const textInput=document.getElementById("text-input").value.toUpperCase();
+  const textInput=document.getElementById("text-input").value;
   const offsetNumber=Number(document.getElementById("offset").value);
 
   const output= cipher.encode(offsetNumber, textInput);
@@ -16,7 +16,7 @@ document.getElementById("decode").addEventListener("click", function(event){
 
   event.preventDefault();
 
-  const textInput=document.getElementById("text-input").value.toUpperCase();
+  const textInput=document.getElementById("text-input").value;
   const offsetNumber=Number(document.getElementById("offset").value);
 
   const output= cipher.decode(offsetNumber, textInput);
